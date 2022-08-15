02d7a7129b6472f7a27dbf2c532492dd957ac653
Folarin Salami <70457087+folarinsalami@users.noreply.github.com>
2022-08-15 17:47:35 +0100

    Add files via upload

---
+++ Navbar Project/navs.js
@@ -0,0 +1,26 @@
+// classList - shows/gets all classes
+// contains- checks classList for specific class
+//add - add class
+// remove - remove class
+// toggle - toggle class
+
+const navToggle=document.querySelector(".nav-toggle");
+const links=document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+   links.classList.toggle("show-links");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
